8bda32aa4dca8309f04954d081afab30cc7333c9
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-08-27 09:54:08 +0600

    DR TC problem

---
+++ A_Dr_TC.cpp
@@ -0,0 +1,42 @@
+/*
+Problem Link
+https://codeforces.com/problemset/problem/2106/A
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int cnt = 0;
+        for(char c : s)
+        {
+            if(c == '1')
+                cnt ++;
+        }
+
+        int ans = 0;
+
+        for(char c : s)
+        {
+            if(c == '1')
+                ans += cnt-1;
+            else
+                ans += cnt+1;
+        }
+
+        cout << ans << endl;
+
+    }
+    return 0;
+}
